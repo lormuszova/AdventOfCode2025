80aa19315018fa95a1de70e013394a2e6b7ccb7e
lormuszova <danka1992@gmail.com>
2025-12-01 22:47:27 +0100

    Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,49 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val directions = processInput(input)
+        var result = 0
+        var actualPosition = 50
+        directions.forEach {
+            actualPosition = (actualPosition + it).mod(100)
+            if (actualPosition == 0) {
+                result++
+            }
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val directions = processInput(input)
+        var result = 0
+        var actualPosition = 50
+        var nextPosition: Int
+        directions.forEach {steps ->
+            val remainder = steps.rem(100)
+            val dividedSteps = steps.div(100)
+            result += dividedSteps.absoluteValue
+            nextPosition = actualPosition + remainder
+
+            if(nextPosition<= 0 && actualPosition != 0) result++
+            if(nextPosition>=100) result++
+
+            actualPosition = nextPosition.mod(100)
+        }
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun processInput(input: List<String>): List<Int> {
+    return input.map {
+        it.replace("R", "").replace("L", "-").toInt()
+    }
 }
