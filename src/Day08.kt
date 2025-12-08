f37aa468ee3f0086f86acf4015f229e07c3454e0
lormuszova <danka1992@gmail.com>
2025-12-08 14:29:25 +0100

    Day08.kt - small refactor

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,22 +1,14 @@
 import utils.Coordinate3D
 
 fun main() {
-    fun part1(input: List<String>, connections: Int): Int {
-        return Day08.part1(input, connections)
-    }
-
-    fun part2(input: List<String>): Long {
-        return Day08.part2(input)
-    }
-
     val testInput = readInput("Day08_test")
-    check(part1(testInput, 10) == 40)
-    check(part2(testInput) == 25272L)
+    check(Day08.part1(testInput, 10) == 40)
+    check(Day08.part2(testInput) == 25272L)
 
     // Read the input from the `src/Day08.txt` file.
     val input = readInput("Day08")
-    part1(input, 1000).println()
-    part2(input).println()
+    Day08.part1(input, 1000).println()
+    Day08.part2(input).println()
 }
 
 object Day08 {
@@ -31,20 +23,15 @@ object Day08 {
             }
         }.sortedBy { it.third }
         val circuits = coordinates.map { mutableSetOf(it) }.toMutableList()
-        var connectionsMade = 0
 
-        for ((c1, c2, _) in distances) {
-
-            val s1 = circuits.find { it.contains(c1) }
-            val s2 = circuits.find { it.contains(c2) }
+        for ((c1, c2, _) in distances.take(connections)) {
+            val s1 = circuits.first { it.contains(c1) }
+            val s2 = circuits.first { it.contains(c2) }
 
             if (s1 !== s2) {
-                s1!!.addAll(s2!!)
+                s1.addAll(s2)
                 circuits.remove(s2)
             }
-
-            connectionsMade++
-            if (connectionsMade == connections) break
         }
         val largestCircuits = circuits.map { it.size }.sortedDescending().take(3)
         return largestCircuits.reduce { acc, i -> acc * i }
@@ -65,11 +52,11 @@ object Day08 {
 
         for ((c1, c2, _) in distances) {
 
-            val s1 = circuits.find { it.contains(c1) }
-            val s2 = circuits.find { it.contains(c2) }
+            val s1 = circuits.first { it.contains(c1) }
+            val s2 = circuits.first { it.contains(c2) }
 
             if (s1 !== s2) {
-                s1!!.addAll(s2!!)
+                s1.addAll(s2)
                 circuits.remove(s2)
             }
 
